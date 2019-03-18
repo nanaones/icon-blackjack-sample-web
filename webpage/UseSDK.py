9d01c54aa99138fdc5f8fa3035209c985c93b9bf
nanaones <nara03050@gmail.com>
2019-03-18 17:51:41 +0900

    불필요부분 삭제

--- webpage/UseSDK.py
+++ webpage/UseSDK.py
@@ -9,6 +9,7 @@ DIR_PATH = os.path.abspath(os.path.dirname('__file__'))
 
 class JsonRPCCalls():
 
+    # SCORE address and EOA address for call
     def __init__(self, keywallet_address = "hxbbf22a5733d5afd3743d4fd39dc9448f481c8f15" , sample_game_score_address = "cx89245b4a663f2062a9fe52a219c44c281e1d6c36"):
         self._sample_game_score_address = sample_game_score_address
         self._keywallet_address = keywallet_address
